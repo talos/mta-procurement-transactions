d98a8853aba9a4b4b357a9b62c55301ec8d51d40
John Krauss <irving.krauss@gmail.com>
2014-08-17 00:27:49 -0400

    Fixing data quality issues by type checking column data.  If the data doesn't match, it's passed to the next column.

--- bin/parse.py
+++ bin/parse.py
@@ -7,42 +7,66 @@ import re
 import codecs
 
 
-COLUMNS = (
-    u'page_number',
-    u'vendor_number',
-    u'vendor_name',
-    u'transaction_number',
-    u'procurement_description',
-    u'amount_expended_for_fiscal_year',
-    u'amount_expended_for_life_to_date',
-    u'does_the_contract_have_an_end_date',
-    u'current_or_outstanding_balance',
-    u'number_of_bids_or_proposals_received_prior_to_award_of_contract',
-    u'is_the_vendor_a_nys_or_foreign_business_enterprise?',
-    u'is_the_vendor_a_minority_or_woman-owned_business_enterprise?',
-    u'exemption_from_the_publication_requirements_of_article_4c_of_the_economic-development_law?',
-    u'if_yes,_basis_for_exemption',
-    u'type_of_procurement',
-    u'award_process',
-    u'award_date',
-    u'begin_date',
-    u'renewal_date',
-    u'end_date',
-    u'amount',
-    u'fair_market_value',
-    u'explain_why_the_fair_market_value_is_less_than_the_contract_amount',
-    u'status',
-    u'were_mwbe_firms_solicited_as_part_of_this_procurement_proces',
-    u'number_of_bids_and_proposal_received_from_mwbe_firms',
-    u'address_line1',
-    u'address_line2',
-    u'city',
-    u'state',
-    u'postal_code',
-    u'plus_4',
-    u'province_region',
-    u'country'
-)
+transaction_number = lambda s: re.search('^[0-9\-]+$', s) or s == u''
+integer = lambda s: re.search(r'^\d+$', s) or s == u''
+money = lambda s: re.search(r'^-?\$[0-9,]+\.?\d*$', s) or s in (u'', u'****************')
+text = lambda s: True
+date = lambda s: re.search(r'^\d{2}/\d{2}/\d{4}$', s) or s == u''
+options = lambda *options: lambda s: s.lower() in [o.lower() for o in options]
+yes_or_no = options('yes', 'no', '')
+
+COLUMNS = dict((
+    (u'page_number', integer),
+    (u'vendor_number', integer),
+    (u'vendor_name', text),
+    (u'transaction_number', transaction_number),
+    (u'procurement_description', text),
+    (u'amount_expended_for_fiscal_year', money),
+    (u'amount_expended_for_life_to_date', money),
+    (u'does_the_contract_have_an_end_date', yes_or_no),
+    (u'current_or_outstanding_balance', money),
+    (u'number_of_bids_or_proposals_received_prior_to_award_of_contract', integer),
+    (u'is_the_vendor_a_nys_or_foreign_business_enterprise?', options(u'foreign',
+                                                                     u'nys',
+                                                                     u'')),
+    (u'is_the_vendor_a_minority_or_woman-owned_business_enterprise?', yes_or_no),
+    (u'exemption_from_the_publication_requirements_of_article_4c_of_the_economic-development_law?', yes_or_no),
+    (u'if_yes,_basis_for_exemption', text),
+    (u'type_of_procurement', options(u'Staffing Services',
+                                     u'Design and Construction/Maintenance',
+                                     u'Commodities/Supplies',
+                                     u'Consulting Services',
+                                     u'Other',
+                                     u'Technology - Hardware',
+                                     u'Other Professional Services',
+                                     u'Technology - Consulting/Development or Sup',
+                                     u'Technology - Software',
+                                     u'Legal Services',
+                                     u'Telecommunication Equipment or Services',
+                                     u'Financial Services')),
+    (u'award_process', options(u'Authority Contract - Competitive Bid',
+                               u'Non Contract Procurement/Purchase Order',
+                               u'Purchased Under State Contract',
+                               u'Authority Contract - Non-Competitive Bid')),
+    (u'award_date', date),
+    (u'begin_date', date),
+    (u'renewal_date', date),
+    (u'end_date', date),
+    (u'amount', money),
+    (u'fair_market_value', money),
+    (u'explain_why_the_fair_market_value_is_less_than_the_contract_amount', text),
+    (u'status', options('open', 'completed')),
+    (u'were_mwbe_firms_solicited_as_part_of_this_procurement_proces', yes_or_no),
+    (u'number_of_bids_and_proposal_received_from_mwbe_firms', integer),
+    (u'address_line1', text),
+    (u'address_line2', text),
+    (u'city', text),
+    (u'state', text),
+    (u'postal_code', text),
+    (u'plus_4', text),
+    (u'province_region', text),
+    (u'country', text)
+))
 
 
 def locate(pattern, page):
@@ -66,7 +90,7 @@ def parse(infile):
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    writer = csv.DictWriter(codecs.getwriter('utf8')(sys.stdout), COLUMNS)
+    writer = csv.DictWriter(codecs.getwriter('utf8')(sys.stdout), COLUMNS.keys())
     pages = infile.read().split('Procurement Transactions Listing:')
 
     writer.writeheader()
@@ -81,7 +105,7 @@ def parse(infile):
         rh, rd = locate(r'Type of Procurement:\s+\S', page)
 
         # Accumulators
-        lha, lda, rha, rda = (u'', u'', u'', u'')
+        lha, lda, nlda, rha, rda, nrda = (u'', u'', u'', u'', u'', u'')
 
         _ = lambda x: x.strip().replace(' ', '_').lower()
         __ = lambda x, y: (_(x) + u'_' + _(y)).strip('_')
@@ -114,13 +138,21 @@ def parse(infile):
 
             if _(lha) in COLUMNS and not __(lha, left_header) in COLUMNS:
                 output[_(lha)] = process_data(lda)
+                if not COLUMNS[_(lha)](lda):
+                    raise Exception(u'Value "{}" invalid in column "{}"'.format(
+                        lda, _(lha)))
                 lha = u''
-                lda = u''
+                lda = nlda
+                nlda = u''
 
             if _(rha) in COLUMNS and not __(rha, right_header) in COLUMNS:
                 output[_(rha)] = process_data(rda)
+                if not COLUMNS[_(rha)](rda):
+                    raise Exception(u'Value "{}" invalid in column "{}"'.format(
+                        rda, _(rha)))
                 rha = u''
-                rda = u''
+                rda = nrda
+                nrda = u''
 
             if left_header:
                 lha = (lha + u' ' + left_header).strip(' ')
@@ -129,10 +161,22 @@ def parse(infile):
                 rha = (rha + u' ' + right_header).strip(' ')
 
             if left_data:
-                lda = (lda + u' ' + left_data).strip(' ')
+                if _(lha) in COLUMNS:
+                    if COLUMNS[_(lha)]((lda + u' ' + left_data).strip(' ')):
+                        lda = (lda + u' ' + left_data).strip(' ')
+                    else:
+                        nlda = (nlda + u' ' + left_data).strip(' ')
+                else:
+                    lda = (lda + u' ' + left_data).strip(' ')
 
             if right_data:
-                rda = (rda + u' ' + right_data).strip(' ')
+                if _(rha) in COLUMNS:
+                    if COLUMNS[_(rha)]((rda + u' ' + right_data).strip(' ')):
+                        rda = (rda + u' ' + right_data).strip(' ')
+                    else:
+                        nrda = (nrda + u' ' + right_data).strip(' ')
+                else:
+                    rda = (rda + u' ' + right_data).strip(' ')
 
         writer.writerow(output)
 
